bdc31822206eb16a6526345e5f34273337aad046
octaviofranceschetto <octaviofranceschetto@gmail.com>
2024-06-29 19:47:59 -0300

    inserção dos icones de pesquisa e da barra lateral de navegação

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    var nomeUsuario = username;
     
     // Aqui você pode adicionar a lógica para validar o usuário e senha
     if(username && password === 'admin') {
-        localStorage.setItem('nomeUsuario', nomeUsuario);
         window.location.href = 'home.html';
     } else {
         alert('Usuário ou senha inválidos');
